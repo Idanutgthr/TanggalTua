218ba8034a90e357c42ef5ccb66aee32e7878e16
Idanutgthr <wibowowildan15@gmail.com>
2026-06-01 19:25:28 +0700

    TESTTTT

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 
-// GANTI DENGAN KONFIGURASI FIREBASE PROJECT ANDA SENDIRI
 // Menyembunyikan Key menggunakan Environment Variables bawaan hosting/Vite
-//testtt
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY || window._env_?.FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || window._env_?.FIREBASE_AUTH_DOMAIN,
@@ -17,8 +15,5 @@ const firebaseConfig = {
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-
-// Ekspor instance Auth dan Google Provider untuk dipakai di app.js
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
